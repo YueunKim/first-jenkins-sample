f01a0bca79c94f206e568232cf8b88e1d17a80d7
yueunkim <yueunkim0605@gmail.com>
2024-03-05 16:50:01 +0900

    webhook last test

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello Hi YunieYunie!!!!!, git webhook4";
+        return "Hello Hi YunieYunie!!!!!, git webhook last test";
     }
 }
